e5cd8e6a58583b2f973bf73ef9b0c415477f132e
Pavel Novikov <spl.novikov@gmail.com>
2018-08-01 13:43:25 +0400

    adds pick image

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import { Button, View, Image, StyleSheet } from 'react-native';
-
-import imagePlaceholder from '../../assets/sb.png';
+import ImagePicker from 'react-native-image-picker';
 
 
 class PickImage extends Component {
+  state = {
+    pickedImage: null
+  };
+
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({ title: 'Pick an Image' }, res => {
+      if (res.didCancel) {
+        console.log('User cancelled!');
+      } else if (res.error) {
+        console.log('Error', res.error);
+      } else {
+        this.setState({
+          pickedImage: { uri: res.uri }
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.placeholder}>
-          <Image source={imagePlaceholder} style={styles.previewImage} />
+          <Image source={this.state.pickedImage} style={styles.previewImage} />
         </View>
         <View style={styles.button}>
-          <Button title='Pick Image' onPress={() => alert('Pick Image!')} />
+          <Button title='Pick Image' onPress={this.pickImageHandler} />
         </View>
       </View>
     );
